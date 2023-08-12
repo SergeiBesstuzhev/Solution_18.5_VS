549a2993bce5f39097e72d708f2b5cb87f066f5e
Sergei Bess <sergeibesstuzhev1989@gmail.com>
2023-08-12 12:04:42 +0300

    Добавьте файлы проекта.

---
+++ Solution_18.5_VS.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+class PlayerList {
+public:
+    PlayerList() : Name("Player"), Score(0) {}
+    PlayerList(std::string _Name, float _Score) : Name(_Name), Score(_Score) {}
+    void ShowParam() {
+        std::cout << Name << " " << Score << "\n";
+    }
+private:
+    std::string Name;
+    float Score;
+};
+
+int main()
+{
+    int iter = 0; 
+    int PlayerCount;
+    std::cout << "How many players to add: ";
+    std::cin >> PlayerCount;
+    
+    std::vector<PlayerList>pArr(PlayerCount);
+
+    std::cout << "Fill the list\n";
+
+    for (int i = 0; i < PlayerCount; i++) {
+        std::string Name;
+        std::cout << "Enter Name: ";
+        std::cin >> Name;
+
+        float Score;
+        std::cout << "Enter Score: ";
+        std::cin >> Score;
+
+        std::cout << std::endl;
+
+        PlayerList p(Name, Score);
+        pArr[i] = p;
+    }
+
+    std::cout << "\t.:Player List:. \n\n";
+
+    for (int i = 0; i < PlayerCount; i++) {
+        pArr[i].ShowParam();
+    }
+}
